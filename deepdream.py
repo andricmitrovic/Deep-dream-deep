54d2f56428a1e8e61ca78daa097f50c23fadbe51
Nikola Andric Mitrovic <andricmitrovicnikola@yahoo.com>
2020-11-13 20:46:02 +0100

    Image pyramid implemented

--- deepdream.py
+++ deepdream.py
@@ -4,13 +4,26 @@ from utils import *
 
 class DeepDreamClass():
     def __init__(self, image: torch.Tensor, layers: list):
-        self.octave_scale = 1.30
-        self.learning_rate = 5e-3
+        # Metaparameters
+        self.learning_rate = 5e-3   # Step_size to use while optimizing image
+        self.scales = 5             # Number of different downsampled scales wrt to original image
+        self.octave_scale = 1.30    # Parameter for downsampling images
+        self.num_iters = 100        # Number of iterations to run at a single scale
+
         self.image = image
         self.model = VGG19_modified(layers)
 
     def deepdream(self):
-        pass
+
+        # Image pyramid, octaves contain the normalized original image at different scales
+        octaves = []
+        orig_shape = self.image.size[::-1]  # PIL.size inverts the width and height
+        for scale in range(self.scales):
+            new_shape = [int(shape * (self.octave_scale ** (-scale))) for shape in orig_shape]
+            tfms = T.Compose([T.Resize(size=new_shape), T.ToTensor(), ut.norm])
+            octaves += [tfms(self.image).unsqueeze(0)]
+        octaves.reverse()   # Reverse octaves because we want to go from the the smallest scaled image to the original
+
 
 
 if __name__ == "__main__":
@@ -22,6 +35,6 @@ if __name__ == "__main__":
     input_img = ut.load_img("./starry_night.jpg")
 
     dream_object = DeepDreamClass(input_img, layers)
-    #output = dream_object.deepdream()
+    output = dream_object.deepdream()
 
-    ut.display_img([input_img, input_img], layers)
+    #ut.display_img([input_img, input_img], layers)
