c2960fb96d6d23a767150f0179643b6e740a46b1
Nikola Andric Mitrovic <andricmitrovicnikola@yahoo.com>
2020-12-05 19:18:41 +0100

    Implemented function for dreaming iteratively and polished the existing code.

--- deepdream.py
+++ deepdream.py
@@ -1,22 +1,20 @@
 from Models.vgg19_modified import VGG19_modified
 from utils import *
-import scipy.ndimage as nd
 from tqdm import tqdm
 import utils
 
 
 class DeepDreamClass():
     def __init__(self, image: torch.Tensor, layers: list):
-        # Metaparameters
+        # Metaparameters, play around with these
+        self.learning_rate = 0.09     # Step_size to use while optimizing image
+        self.num_iters = 5            # Number of iterations to run at a single scale
+        self.num_scales = 4           # Number of scaled down levels of the original image
+        self.scale_coef = 1.4         # Parameter for downsampling image
 
-        self.learning_rate = 0.09      # Step_size to use while optimizing image
-        self.num_iters = 10            # Number of iterations to run at a single scale
+        self.spatial_shift_size = 32  # Number of pixels to randomly shift image before grad ascent
 
-        self.num_scales = 7           # Number of scaled down levels of the original image
-        self.scale_coef = 1.6         # Parameter for downsampling image
-
-        self.spatial_shift_size = 32   # Number of pixels to randomly shift image before grad ascent
-
+        # CascadeGaussianSmoothing gives much better results but I left GaussianBlur for comparison.
         self.smooth_function = "CascadeGaussianSmoothing"   # CascadeGaussianSmoothing or GaussianBlur
         self.smooth_coef = 0.5
 
@@ -45,7 +43,6 @@ class DeepDreamClass():
             h_shift, w_shift = np.random.randint(-self.spatial_shift_size, self.spatial_shift_size + 1, 2)
             image = ut.random_circular_spatial_shift(image, h_shift, w_shift)
 
-
             # Activations at the chosen layer
             activations = self.model(image)
 
@@ -73,16 +70,12 @@ class DeepDreamClass():
             We optimize the image by doing gradient ascent, 
             actually we just add the normalized gradient instead of subtracting like in gradient descent.
             '''
-            # image.data += self.learning_rate / torch.mean(torch.abs(grad)) * grad
             image.data += self.learning_rate * (grad / torch.std(grad))
 
             image.data = ut.clip(image.data)    # Clip image to be in the desired bounds
             image.grad.data.zero_()             # Clear the grad for the next iteration calculations
 
             image = ut.random_circular_spatial_shift(image, h_shift, w_shift, should_undo=True)
-
-            # if i % 50 == 0:
-            #    print(f"After {i} iterations, Loss: {round(tot_loss.item(), 3)}")
 
         # In the main loop all tensor calculations are done on cpu so we return image on cpu with that in mind
         return image.data.cpu()
@@ -91,7 +84,7 @@ class DeepDreamClass():
 
         scale = self.num_scales - (scale + 1)        # Go from the smallest scaled image to the biggest one
 
-        orig_shape = self.image.size[::-1]  # PIL.size inverts the width and height
+        orig_shape = self.image.size[::-1]           # PIL.size inverts the width and height
 
         new_shape = [int(shape * (self.scale_coef ** (-scale))) for shape in orig_shape]
         tfms = T.Compose([T.Resize(size=new_shape), T.ToTensor(), ut.normalize])
@@ -111,11 +104,9 @@ class DeepDreamClass():
         exponent = pyramid_level - pyramid_size + 1
         new_shape = np.round(np.float32(base_shape) * (pyramid_ratio ** exponent)).astype(np.int32)
 
-        #print(new_shape)            # moras shape i onog sranja details da menjas ili ipak ne promeni se posle samo
-
         if new_shape[0] < SHAPE_MARGIN or new_shape[1] < SHAPE_MARGIN:
-            print(
-                f'Pyramid size {pyramid_size} with pyramid ratio {pyramid_ratio} gives too small pyramid levels with size={new_shape}')
+            print(f'Pyramid size {pyramid_size} with pyramid ratio {pyramid_ratio} \
+                    gives too small pyramid levels with size={new_shape}')
             print(f'Please change parameters.')
             exit(0)
 
@@ -143,8 +134,6 @@ class DeepDreamClass():
 
     def deepdream(self):
 
-        # !!!!!!!!!!!!!!!!! Try cv pyramids on results instead of using them on the original image !!!!!!!!!!!
-
         details = self.init_details()  # Init details with 0 tensor with the shape same as the smallest scaled img
         dream_image = None
         input_img = None
@@ -154,8 +143,7 @@ class DeepDreamClass():
                 input_img = self.octave_made_from_original_img(scale)
             elif self.octave_function == "input":
                 input_img = self.octave_made_from_input_img(scale, input_img)
-            #print(details.shape)
-            #print(input_img.shape)
+
             details = torch.tensor(nd.zoom(details, np.array(input_img.shape) / np.array(details.shape), order=1))
 
             enhanced_image = details + input_img
@@ -167,63 +155,77 @@ class DeepDreamClass():
 
         return dream_image
 
+# Displaying images is disabled here since the number of images can be quite high and clog up the RAM
+# and we would need to save them and then load them in batches for display
+def dream_iteratively_through_layers_save(ut, layers, num_of_runs_per_layer = 3, path = None):
+
+    num_of_layers = len(layers)
+    total_num_of_runs = num_of_runs_per_layer *  num_of_layers
+
+    titles = []
+
+    input_img = ut.load_img(path=path, resize=True)
+    width, height = input_img.size
+
+    display_list = [input_img]
+
+    folder_path_save = ut.make_save_dir()
+
+    ut.save_img(input_img, "original", 0, folder_path_save)
+
+    for run in range(1, total_num_of_runs + 1):
+
+        new_layer = layers[int((run - 1) / num_of_runs_per_layer)]
+
+        print(f"\n{new_layer} {run}")
+
+        dream_object = DeepDreamClass(input_img, [new_layer])
+
+        if path is None:
+            dream_object.octave_function = "input"
+
+        output_img = dream_object.deepdream()
+
+        ut.save_img(output_img, new_layer, run, folder_path_save)
+
+        input_img = ut.prepare_new_input_from_output(output_img, zoom_factor=1.005, width=width, height=height)
+
+# Compute one dream image for each given layer, display and optionally save them
+def dream_using_different_layers_display_and_maybe_save(ut, layer, path, save = False):
+
+    titles = []
+    input_img = ut.load_img(path=path, resize=True)
+    display_list = [input_img]
+
+    for layer in layers:
+        print(f"\n{layer}")
+
+        dream_object = DeepDreamClass(input_img, [layer])
+
+        if path is None:
+            dream_object.octave_function = "input"
+
+        output_img = dream_object.deepdream()
+
+        display_list.append(output_img)
+
+    ut.display_img(display_list, titles, save=save)
 
 if __name__ == "__main__":
     # Define which model and layers we are using
     model_name = "vgg19"
 
-
+    # Specify which layers to use for generating dream images. Here I specified all layers for comparison purpose.
     layers = ['conv1_1', 'conv1_2',
               'conv2_1', 'conv2_2',
               'conv3_1', 'conv3_2', 'conv3_3', 'conv3_4',
               'conv4_1', 'conv4_2', 'conv4_3', 'conv4_4',
               'conv5_1', 'conv5_2', 'conv5_3', 'conv5_4']
 
-
-    #layers = ['conv5_2']                        # Specify the layers for the nn to use to generate the dream
-    dream_iteratively = False                   # False if dream is generate with one pass of the program,
-                                                # True if you want the output to be fed back to the program.
-    num_of_runs = 10                            # If dream_iteratively is True set this to the desired number of runs
-
-    path = "Input/leaves.jpg"            # Path of the input image or None for static image
-    #path = None
-
+    # layers = ['conv3_4]
 
     ut = Utils(model_name)
-    input_img = ut.load_img(path)
+    path = "Input/earth.jpg"            # Path of the input image or None for static image
 
-    display_list = [input_img]
-
-    if dream_iteratively:
-
-        layers = layers[0] * num_of_runs
-
-        for run in range(num_of_runs):
-            print(f"\n{layers[run]} {run}")
-
-            print(input_img.size)
-
-            dream_object = DeepDreamClass(input_img, [layers[run]])
-            if path is None:
-                dream_object.octave_function = "input"
-            output_img = dream_object.deepdream()
-            display_list.append(output_img)
-
-            output_img = ut.denormalize(output_img)
-            output_img = (output_img*255).astype(np.uint8)
-
-            #output_img = nd.zoom(output_img, 1.1)
-
-            input_img = Image.fromarray(output_img, 'RGB')
-
-            layers[run] = layers[run] + "_" + str(run)
-    else:
-        for layer in layers:
-            print(f"\n{layer}")
-            dream_object = DeepDreamClass(input_img, [layer])
-            if path is None:
-                dream_object.octave_function = "input"
-            output_img = dream_object.deepdream()
-            display_list.append(output_img)
-
-    ut.display_img(display_list, layers, save=True)
+    #dream_iteratively_through_layers_save(ut, layers, num_of_runs_per_layer=2, path=path)
+    dream_using_different_layers_display_and_maybe_save(ut, layers, path=path)
--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ import cv2 as cv
 from datetime import datetime
 import os
 from scipy.ndimage.filters import gaussian_filter
+import scipy.ndimage as nd
+
 
 import numbers
 import math
@@ -40,14 +42,14 @@ class Utils:
 
         self.mean = np.array([0.485, 0.456, 0.406], dtype=np.float32)
         self.stdv = np.array([0.229, 0.224, 0.225], dtype=np.float32)
-        #self.img_size = 512
-        self.img_size = 600
+        self.img_size = 720
 
-    def load_img(self, path: str = None):
+    def load_img(self, path: str = None, resize: bool = True):
         """
         Loading image function.
 
         :param path: Optional path of the image, default value is None and the dream will be made from noise img.
+        :param resize: Resize image to 600 width or keep the original shape.
         :return: PIL resized image.
         """
         if path is None:
@@ -55,7 +57,8 @@ class Utils:
             img = Image.fromarray(np.random.uniform(low=0, high=255, size=shape).astype(np.uint8), 'RGB')
         else:
             img = Image.open(path)
-            img = self.resize(img)
+            if resize:
+                img = self.resize(img)
 
         return img
 
@@ -78,11 +81,9 @@ class Utils:
         i = 0
         n = len(img_list)
 
-        # Make a dir in Output folder with unique name corresponding to the current date and time
+
         if save:
-            today = datetime.now()
-            path = "./Output/" + today.strftime('%H_%M_%S_%d_%m_%Y')
-            os.mkdir(path)
+            path = self.make_save_dir()
 
         while True:
             if i != 0:
@@ -106,6 +107,39 @@ class Utils:
                 break
 
         cv.destroyAllWindows()
+
+    def make_save_dir(self):
+        """
+        Makes a dir in Output folder with unique name corresponding to the current date and time.
+
+        :return: path of the created folder
+        """
+
+        today = datetime.now()
+        path = "./Output/" + today.strftime('%H_%M_%S_%d_%m_%Y')
+        os.mkdir(path)
+
+        return path
+
+    def save_img(self, img, layer, run, path):
+        """
+        Saves the image with info about its layer and run in which it was generated.
+
+        :param img: Image to be saved.
+        :param layer: Which layer was used to produce the image.
+        :param run: In which run was the image generated
+        :param path: Path of the directory where the image should be stored.
+        :return:
+        """
+
+        if run != 0:
+            cv_img = cv.cvtColor(self.denormalize(img), cv.COLOR_RGB2BGR)
+        else:
+            cv_img = np.array(img)
+            #cv_img = cv.cvtColor(np.array(img), cv.COLOR_RGB2BGR)
+
+        filename = path + "/" + self.model_name + "_" + layer + "_" + str(run) + ".jpg"
+        cv.imwrite(filename, cv_img * 255)  # mul by 255 because our img is in range [0,1]
 
     def clip(self, tensor: torch.Tensor):
         """
@@ -145,6 +179,16 @@ class Utils:
         return tensor.numpy().transpose(1, 2, 0)
 
     def random_circular_spatial_shift(self, tensor, h_shift, w_shift, should_undo=False):
+        """
+        Shifts the elements of the tensor in the given direction.
+        Elements that are shifted beyond the last position are re-introduced at the first position.
+
+        :param tensor: Tensor to be shifted.
+        :param h_shift: Number of pixel to shift horizontally.
+        :param w_shift: Number of pixel to shift vertically.
+        :param should_undo: If True the function will undo the shift.
+        :return:
+        """
         if should_undo:
             h_shift = -h_shift
             w_shift = -w_shift
@@ -154,15 +198,46 @@ class Utils:
             return rolled
 
     def gausian_blur(self, grad: torch.tensor, sigma):
+        """
+        Blurs the gradient.
+
+        :param grad: Grad.
+        :param sigma: Parameter for blurring the gradient.
+        :return:
+        """
         grad = grad.data.cpu()
         grad_1 = gaussian_filter(grad, sigma=sigma * 0.5)
         grad_2 = gaussian_filter(grad, sigma=sigma * 1.0)
         grad_3 = gaussian_filter(grad, sigma=sigma * 2.0)
         return torch.tensor(grad_1 + grad_2 + grad_3, device=device)
 
+    def prepare_new_input_from_output(self, output_img: torch.Tensor, zoom_factor=1.005, width=600, height=800):
+        """
+        Denormalizes and zooms image from the output tensor to be used again in the dreaming process.
+        ( Unnecessary process since we will be converting it again back to tensor and normalizing,
+          but requires some time to rewrite other code. )
 
+        :param output_img: Image to be processed.
+        :param zoom_factor: Zooming factor.
+        :param width: Width before zooming so we can resize it back to that size again.
+        :param height: Height before zooming so we can resize it back to that size again.
+        :return:
+        """
 
+        output_img = self.denormalize(output_img)
+        output_img = (output_img * 255).astype(np.uint8)
+        output_img = nd.zoom(output_img, zoom_factor)
 
+        new_height = height * zoom_factor
+        new_width = width * zoom_factor
+
+        left_limit = int((new_height - height) / 2)
+        right_limit = int((new_width - width) / 2)
+        output_img = output_img[left_limit:height + left_limit, right_limit:width + right_limit]
+
+        return Image.fromarray(output_img, 'RGB')
+
+# Copied from another DeepDream implementation repo.
 class CascadeGaussianSmoothing(torch.nn.Module):
     """
     Apply gaussian smoothing seperately for each channel (depthwise convolution)
